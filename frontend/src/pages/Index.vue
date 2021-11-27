55d39224099d3761463a9a9ca8a41a8a17f9cef9
Trinmar Boado <timi.boado@gmail.com>
2021-11-27 19:35:39 +0800

    Initial commit

---
+++ frontend/src/pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    <q-btn @click="$router.push('/todo')">Todo App</q-btn>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
---
+++ frontend/src/pages/Todo.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <q-toolbar class="bg-primary text-white">
+      <q-btn flat round dense icon="menu" class="q-mr-sm" />
+      <q-avatar>
+        <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+      </q-avatar>
+
+      <q-toolbar-title>Batch 38 Todo App {{ description }}</q-toolbar-title>
+
+      <q-btn flat round dense icon="whatshot" @click="$router.push('/')" />
+    </q-toolbar>
+    <div class="q-ma-md">
+      <q-input @keyup.enter="addTask" v-model="description" label="type your task here" />
+      <q-btn icon="add_circle" label="add" outline color="primary" @click="addTask"  />
+      <q-list bordered separator>
+        <q-item v-for="(todo) in filteredTodos" :key="todo.id" clickable v-ripple @blur="hovering = null" @mouseover="hovering = todo.id">
+          <q-item-section avatar>
+            <q-checkbox v-model="todo.isDone" />
+          </q-item-section>
+          <q-item-section :class="{ 'text-strike': todo.isDone }">
+            {{ todo.desc }}
+          </q-item-section>
+          <q-item-section side>
+            <q-btn @click="deleteTask(todo)" round icon="delete" size="sm" color="red" v-show="hovering === todo.id" />
+          </q-item-section>
+        </q-item>
+      </q-list>
+      <div>
+        {{ itemsLeft }} items left
+
+        <q-btn-toggle
+          v-model="showBy"
+          toggle-color="primary"
+          :options="[
+            {label: 'All', value: 'all'},
+            {label: 'Active', value: 'active'},
+            {label: 'Completed', value: 'completed'}
+          ]"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      showBy: 'all',
+      hovering: null,
+      description: 'hello',
+      todos: [
+        {
+          id: 1,
+          isDone: false,
+          desc: 'create input box asd'
+        },
+        {
+          id: 2,
+          isDone: false,
+          desc: 'create save button'
+        }
+      ]
+    }
+  },
+  methods: {
+    addTask () {
+      if (this.description) {
+        this.todos.unshift({
+          id: Date.now(),
+          isDone: false,
+          desc: this.description
+        })
+        this.description = ''
+      }
+    },
+    deleteTask: function (t) {
+      const i = this.todos.findIndex(todo => t.id === todo.id)
+      this.todos.splice(i, 1)
+    }
+  },
+  computed: {
+    itemsLeft () {
+      // return this.todos.filter(todo => {
+      //   return todo.isDone === false
+      // }).length
+      return this.todos.filter(t => !t.isDone).length
+    },
+    filteredTodos () {
+      switch (this.showBy) {
+        case 'completed':
+          return this.todos.filter(t => t.isDone)
+        case 'active':
+          return this.todos.filter(t => !t.isDone)
+        default:
+          return this.todos
+      }
+    }
+  }
+}
+</script>
